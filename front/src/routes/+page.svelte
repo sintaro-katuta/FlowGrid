fd9f877500c734dcee1947e269983ae93ced177f
sintaro-katuta <sintaro.katuta@gmail.com>
2025-06-22 23:36:29 +0900

    必須パッケージインストール(#14)

--- front/src/routes/+page.svelte
+++ front/src/routes/+page.svelte
@@ -1,2 +1,7 @@
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<button class="btn">button</button>
+
+<script>
+  import "../app.css"
+</script>
