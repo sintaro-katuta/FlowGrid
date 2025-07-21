ffb575f644028c01d105561061aa3452675711d0
sintaro-katuta <sintaro.katuta@gmail.com>
2025-07-21 18:12:11 +0900

    lint

--- front/vite.config.js
+++ front/vite.config.js
@@ -1,18 +1,18 @@
-import devtoolsJson from 'vite-plugin-devtools-json';
-import tailwindcss from '@tailwindcss/vite';
-import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
-import path from 'path';
+import devtoolsJson from "vite-plugin-devtools-json";
+import tailwindcss from "@tailwindcss/vite";
+import { sveltekit } from "@sveltejs/kit/vite";
+import { defineConfig } from "vite";
+import path from "path";
 
 export default defineConfig({
 	plugins: [tailwindcss(), sveltekit(), devtoolsJson()],
 	server: {
 		host: true,
-		port: 3000
+		port: 3000,
 	},
 	resolve: {
 		alias: {
-			'@': path.resolve(__dirname, 'src')
-		}
-	}
+			"@": path.resolve(__dirname, "src"),
+		},
+	},
 });
